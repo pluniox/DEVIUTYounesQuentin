74de98e97d3d3f23b1f9f81d6e9fd474be9647b6
chakrouny3 <rrer6630@gmail.com>
2024-09-18 19:36:35 +0200

    premier push vrai

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class Media {
+    private string title;
+    private int reference;
+    private int nbExemplaire;
+
+    public Media(string title, int reference, int nbExemplaire) {
+        this.title = title;
+        this.reference = reference;
+        this.nbExemplaire = nbExemplaire;
+        Console.WriteLine("Bonjour");
+    }
+}
+
+class Program {
+    public static void Main(string[] args) {
+        Media media = new Media("Titre", 12134, 5);
+    }
+}
