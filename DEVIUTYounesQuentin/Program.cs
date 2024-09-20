5407a3f58e84c2829a88b48e16d2d292524bc571
chakrouny3 <rrer6630@gmail.com>
2024-09-20 19:16:54 +0200

    test

--- DEVIUTYounesQuentin/Program.cs
+++ DEVIUTYounesQuentin/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class Media {
 
-    protected string title;
+    public string title;
     protected int reference;
     public int nbExemplaire;
 
@@ -84,13 +84,78 @@ public class CD : Media {
 public class Library {
 
     protected List<Media> medias;
+    protected List<Emprunt> emprunts;
 
     public Library() {
         medias = new List<Media>();
+        emprunts = new List<Emprunt>();
     }
 
     public void AjouterMedia(Media media) {
-        medias.Add(media);
+        media += 1;
+    }
+
+    public void RetirerMedia(Media media) {
+        if (media != null && media.nbExemplaire > 0) {
+            media-=1;
+        }
+    }
+
+    public bool EmprunterMedia(int reference, string utilisateur) {
+        Media media = this[reference];
+        if (media != null && media.nbExemplaire > 0) {
+            media.nbExemplaire--;
+            emprunts.Add(new Emprunt(media, utilisateur));
+            return true;
+        }
+        return false;
+    }
+
+    public bool RetournerMedia(int reference, string utilisateur) {
+        Emprunt emprunt = emprunts.Find(e => e.Media.GetReference() == reference && e.Utilisateur == utilisateur);
+        if (emprunt != null) {
+            emprunt.Media.nbExemplaire++;
+            emprunts.Remove(emprunt);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Media> RechercherMedia(string critere) {
+        List<Media> resultats = new List<Media>();
+        foreach (var media in medias) {
+            if (media is Livre livre && (livre.title.Contains(critere) || livre.Auteur.Contains(critere))) {
+                resultats.Add(livre);
+            }
+            else if (media.title.Contains(critere)) {
+                resultats.Add(media);
+            }
+        }
+        return resultats;
+    }
+
+    public List<Media> ListerMediasEmpruntes(string utilisateur) {
+        List<Media> empruntes = new List<Media>();
+        foreach (var emprunt in emprunts) {
+            if (emprunt.Utilisateur == utilisateur) {
+                empruntes.Add(emprunt.Media);
+            }
+        }
+        return empruntes;
+    }
+
+    public void AfficherStatistiques() {
+        int totalMedias = medias.Count;
+        int totalExemplaires = 0;
+        int exemplairesEmpruntes = emprunts.Count;
+
+        foreach (var media in medias) {
+            totalExemplaires += media.nbExemplaire;
+        }
+
+        Console.WriteLine($"Total de médias: {totalMedias}");
+        Console.WriteLine($"Total d'exemplaires disponibles: {totalExemplaires}");
+        Console.WriteLine($"Nombre d'exemplaires empruntés: {exemplairesEmpruntes}");
     }
 
     public Media this[int reference] {
@@ -105,26 +170,58 @@ public class Library {
     }
 }
 
+
+
+
+public class Emprunt {
+    public Media Media;
+    public string Utilisateur;
+    public DateTime DateEmprunt;
+
+    public Emprunt(Media media, string utilisateur) {
+        Media = media;
+        Utilisateur = utilisateur;
+        DateEmprunt = DateTime.Now;
+    }
+}
+
+
 class Program {
     public static void Main(string[] args) {
 
+        Library library = new Library();
+
         Livre livre = new Livre("Siderman", 1234, 10, "Younes");
-        livre.nbExemplaire += 3;
-        livre.AfficherInfos();
-
-        Console.WriteLine();
-
         DVD dvd = new DVD("Animals 5", 5678, 5, 2.5);
-        dvd.AfficherInfos();
-
-        Console.WriteLine();
-
         CD cd = new CD("Sonate au clair de lune", 9876, 15, "Beethoven");
+
+      /*library.AjouterMedia(livre);
+        library.AjouterMedia(dvd);
+        library.AjouterMedia(cd);
+        */
+
+
+       library.RetirerMedia(livre);
+
+        if (library.EmprunterMedia(1234, "Utilisateur1")) {
+            Console.WriteLine("Média emprunté avec succès !");
+        }
+
+/*        var resultatsRecherche = library.RechercherMedia("Younes");
+        foreach (var media in resultatsRecherche) {
+            media.AfficherInfos();
+        }
+
+        var empruntes = library.ListerMediasEmpruntes("Utilisateur1");
+        foreach (var media in empruntes) {
+            media.AfficherInfos();
+        }
+
+        library.AfficherStatistiques();
+        */
+        
+        livre.AfficherInfos();
+        dvd.AfficherInfos();
         cd.AfficherInfos();
-
-        Console.WriteLine();
-
-
-
     }
 }
