d81bac987a2cb77502505bcbded29d592a4a3da3
chakrouny3 <rrer6630@gmail.com>
2024-09-20 18:57:12 +0200

    Partie 3 2

--- DEVIUTYounesQuentin/Program.cs
+++ DEVIUTYounesQuentin/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 public class Media {
 
     protected string title;
     protected int reference;
-    protected int nbExemplaire;
-    public int NombreExemplairesDisponibles { get; set; }
+    public int nbExemplaire;
 
     public Media(string title, int reference, int nbExemplaire) {
         this.title = title;
@@ -13,49 +13,49 @@ public class Media {
         this.nbExemplaire = nbExemplaire;
     }
 
-    // Surcharge de l'opérateur +
     public static Media operator +(Media media, int nombreExemplaires)
     {
-        media.NombreExemplairesDisponibles += nombreExemplaires;
+        media.nbExemplaire += nombreExemplaires;
         return media;
     }
 
     // Surcharge de l'opérateur -
     public static Media operator -(Media media, int nombreExemplaires)
     {
-        if (media.NombreExemplairesDisponibles - nombreExemplaires < 0)
+        if (media.nbExemplaire - nombreExemplaires < 0)
         {
             throw new InvalidOperationException("Pas assez d'exemplaires disponibles pour effectuer cette opération.");
         }
 
-        media.NombreExemplairesDisponibles -= nombreExemplaires;
+        media.nbExemplaire -= nombreExemplaires;
         return media;
      }
 
      public virtual void AfficherInfos()
-        {
-            Console.WriteLine($"Titre : {title}, Référence : {reference}, Exemplaires disponibles : {NombreExemplairesDisponibles}");
-        }
+     {
+         Console.WriteLine($"Titre : {title}, Référence : {reference}, Exemplaires disponibles : {nbExemplaire}");
+     }
 
+     public int GetReference()
+     {
+         return reference;
+     }
 }
 
 public class Livre : Media {
-
     public string Auteur;
-
 
     public Livre(string title, int reference, int nbExemplaire, string auteur)
         : base(title, reference, nbExemplaire) {
         this.Auteur = auteur;
     }
 
-    public void AfficherInfos() {
+    public override void AfficherInfos() {
         Console.WriteLine($"Auteur: {Auteur}, Titre : {title}, Référence : {reference}, Nombre Exemplaire : {nbExemplaire}");
     }
 }
 
 public class DVD : Media {
-
     public double Duree;
 
     public DVD(string title, int reference, int nbExemplaire, double duree)
@@ -63,13 +63,12 @@ public class DVD : Media {
         this.Duree = duree;
     }
 
-    public void AfficherInfos() {
+    public override void AfficherInfos() {
         Console.WriteLine($"Durée: {Duree} heures, Titre : {title}, Référence : {reference}, Nombre Exemplaire : {nbExemplaire}");
     }
 }
 
 public class CD : Media {
-
     public string Artiste;
 
     public CD(string title, int reference, int nbExemplaire, string artiste)
@@ -77,15 +76,40 @@ public class CD : Media {
         this.Artiste = artiste;
     }
 
-    public void AfficherInfos() {
+    public override void AfficherInfos() {
         Console.WriteLine($"Artiste: {Artiste}, Titre : {title}, Référence : {reference}, Nombre Exemplaire : {nbExemplaire}");
+    }
+}
+
+public class Library {
+
+    protected List<Media> medias;
+
+    public Library() {
+        medias = new List<Media>();
+    }
+
+    public void AjouterMedia(Media media) {
+        medias.Add(media);
+    }
+
+    public Media this[int reference] {
+        get {
+            foreach (var media in medias) {
+                if (media.GetReference() == reference) {
+                    return media;
+                }
+            }
+            return null;
+        }
     }
 }
 
 class Program {
     public static void Main(string[] args) {
 
-        Livre livre = new Livre("Siderman", 1234, 10, "younes");
+        Livre livre = new Livre("Siderman", 1234, 10, "Younes");
+        livre.nbExemplaire += 3;
         livre.AfficherInfos();
 
         Console.WriteLine();
@@ -97,5 +121,10 @@ class Program {
 
         CD cd = new CD("Sonate au clair de lune", 9876, 15, "Beethoven");
         cd.AfficherInfos();
+
+        Console.WriteLine();
+
+
+
     }
 }
