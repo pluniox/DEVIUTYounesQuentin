643870972a52788d9be48eee2e876eee69c765c0
chakrouny3 <rrer6630@gmail.com>
2024-09-20 18:30:50 +0200

    Partie 1

--- Program.cs
+++ Program.cs
@@ -1,20 +1,75 @@
 using System;
 
 public class Media {
-    private string title;
-    private int reference;
-    private int nbExemplaire;
+
+    protected string title;
+    protected int reference;
+    protected int nbExemplaire;
 
     public Media(string title, int reference, int nbExemplaire) {
         this.title = title;
         this.reference = reference;
         this.nbExemplaire = nbExemplaire;
-        Console.WriteLine("Bonjour");
+    }
+}
+
+public class Livre : Media {
+
+    public string Auteur;
+
+
+    public Livre(string title, int reference, int nbExemplaire, string auteur)
+        : base(title, reference, nbExemplaire) {
+        this.Auteur = auteur;
+    }
+
+    public void AfficherInfos() {
+        Console.WriteLine($"Auteur: {Auteur}, Titre : {title}, Référence : {reference}, Nombre Exemplaire : {nbExemplaire}");
+    }
+}
+
+public class DVD : Media {
+
+    public double Duree;
+
+    public DVD(string title, int reference, int nbExemplaire, double duree)
+        : base(title, reference, nbExemplaire) {
+        this.Duree = duree;
+    }
+
+    public void AfficherInfos() {
+        Console.WriteLine($"Durée: {Duree} heures, Titre : {title}, Référence : {reference}, Nombre Exemplaire : {nbExemplaire}");
+    }
+}
+
+public class CD : Media {
+
+    public string Artiste;
+
+    public CD(string title, int reference, int nbExemplaire, string artiste)
+        : base(title, reference, nbExemplaire) {
+        this.Artiste = artiste;
+    }
+
+    public void AfficherInfos() {
+        Console.WriteLine($"Artiste: {Artiste}, Titre : {title}, Référence : {reference}, Nombre Exemplaire : {nbExemplaire}");
     }
 }
 
 class Program {
     public static void Main(string[] args) {
-        Media media = new Media("Titre", 12134, 5);
+
+        Livre livre = new Livre("Siderman", 1234, 10, "younes");
+        livre.AfficherInfos();
+
+        Console.WriteLine();
+
+        DVD dvd = new DVD("Animals 5", 5678, 5, 2.5);
+        dvd.AfficherInfos();
+
+        Console.WriteLine();
+
+        CD cd = new CD("Sonate au clair de lune", 9876, 15, "Beethoven");
+        cd.AfficherInfos();
     }
 }
